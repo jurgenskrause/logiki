11415951189ba8eece90f47e30e8df2886c21156
Jurgens Krause <jurgenskrause@gmail.com>
2026-04-02 15:40:52 +0100

    refactor: improve readability of clue descriptions by splitting sentences with line breaks

--- src/engine/ClueDescriber.ts
+++ src/engine/ClueDescriber.ts
@@ -120,12 +120,12 @@ export function describeRule(clue: ActiveClue): string {
     }
     case 'SEQUENCE_THREE': {
       const [a, b, c] = p.map(x => icon(x.row, x.item));
-      return `${a} and ${b} and ${c} are in three adjacent columns, ${b} is in the middle. <nl> A and c may be reversed.`;
+      return `${a} and ${b} and ${c} are in three adjacent columns. <nl> ${b} is in the middle. ${a} and ${c} may be reversed.`;
     }
     case 'GAPPED_NOT_MIDDLE':
     case 'GAPPED_EXCLUSION': {
       const [a, c, b] = p.map(x => icon(x.row, x.item));
-      return `${a} and ${c} are separated by one column, ${b} cannot be in that column. <nl> A and c may be reversed.`;
+      return `${a} and ${c} are separated by one column. <nl> ${b} cannot be in that column. ${a} and ${c} may be reversed.`;
     }
     case 'DISJUNCTIVE_XOR':
     case 'VERTICAL_DISJUNCTIVE_EXCLUSION': {
